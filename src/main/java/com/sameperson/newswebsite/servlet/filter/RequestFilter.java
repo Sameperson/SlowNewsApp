0de2ced20e3208615d766eb2d3312726b18e434b
Saem <ottosigma@gmail.com>
2016-03-29 16:23:07 +0300

    Added filter

---
+++ src/main/java/com/sameperson/newswebsite/servlet/filter/RequestFilter.java
@@ -0,0 +1,34 @@
+package com.sameperson.newswebsite.servlet.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+
+@WebFilter("/*")
+public class RequestFilter implements javax.servlet.Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletReq, ServletResponse servletResp, FilterChain filterChain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) servletReq;
+
+        System.out.println("Request from " + req.getRemoteAddr()
+                + "; Requested path: " + req.getServletPath()
+                + "; Method: " + req.getMethod());
+
+        filterChain.doFilter(servletReq, servletResp);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
