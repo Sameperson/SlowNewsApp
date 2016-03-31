8592ffea7c9d5fe1cecffb90d04a8237478bbb97
Saem <ottosigma@gmail.com>
2016-03-31 22:35:51 +0300

    tiny fix

--- src/main/java/com/sameperson/newswebsite/model/NewsGenerator.java
+++ src/main/java/com/sameperson/newswebsite/model/NewsGenerator.java
@@ -30,7 +30,6 @@ public class NewsGenerator {
 
     private String generateName() {
         StringBuilder sb = new StringBuilder();
-        Random random = new Random();
         for (int i = 0; i < 20; i++) {
             char c = chars[random.nextInt(chars.length)];
             sb.append(c);
