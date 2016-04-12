6560fffe83b2b1677f9707245cfd96c7509a0abd
Saem <ottosigma@gmail.com>
2016-04-12 20:25:11 +0300

    removed uri push

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -9,7 +9,6 @@ $(function () {
     enableExpandableNews();
     $('.header-link a, .logo, #user-list-link').click(function(event){
         event.preventDefault();
-        window.history.pushState('Object', 'Title', $(this).attr('href'));
         $.ajax({url: $(this).attr('href'), success: function(result){
             document.getElementById("content").innerHTML = result;
             enableExpandableNews();
